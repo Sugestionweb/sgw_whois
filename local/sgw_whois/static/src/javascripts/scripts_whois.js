d6c69aa58c55c17b9ba541568533c2e3ee17524c
javierobcn <javierobcn@gmail.com>
2021-01-03 20:44:41 +0100

    Whois always visible

--- local/sgw_whois/static/src/javascripts/scripts_whois.js
+++ local/sgw_whois/static/src/javascripts/scripts_whois.js
@@ -5,8 +5,7 @@ odoo.define("sgw_whois.tools", function(require) {
   //  var core = require("web.core");
   //  var _t = core._t;
   // Var core = require("web.core");
-
-  let ByID = function(elementId) { 
+  const ByID = function(elementId) { 
     return document.getElementById(elementId); 
   }; 
   
@@ -16,42 +15,44 @@ odoo.define("sgw_whois.tools", function(require) {
 
   
   function render(datos, tld, domain_name) {
-    let domain = domain_name + "." + tld;
+    const domain = domain_name + "." + tld;
     ByID("tabla_result").style.visibility = "visible";
     ByID("loading_table").style.visibility = "hidden";
     ByID("img_resultado_whois_" + tld).innerHTML = datos;
     if (datos.indexOf("text-danger") !== -1) {
       ByID("link_buy_" + tld).innerHTML = "Not available";
-      let to_stroke = document.getElementById("price_" + tld).innerHTML;
-      let stroked = "<s>" + to_stroke + "</s>";
+      const to_stroke = document.getElementById("price_" + tld).innerHTML;
+      const stroked = "<s>" + to_stroke + "</s>";
       ByID("price_" + tld).innerHTML = stroked;
-
-      let Button_Whois = document.createElement("button");
-      Button_Whois.type = "button";
-      Button_Whois.className = "btn btn-info btn-sm";
-      Button_Whois.setAttribute("data-toggle", "modal");
-      Button_Whois.setAttribute("data-target", "#myModal");
-      Button_Whois.textContent = "Whois";
-      Button_Whois.addEventListener("click", function() {
-        ByID("modal_domain_title").innerHTML = domain;
-        ByID("whois_raw").innerHTML = "...Consultando...";
-        $.ajax({
-          type: "POST",
-          global: false,
-          dataType: "html",
-          url: "/get_whois_raw",
-          data: {domain: domain, csrf_token: odoo.csrf_token},
-          success: function(data) {
-            ByID("whois_raw").innerHTML = data;
-          },
-          error: function(jqxhr, status, exception) {
-            // Alert("Exception:", exception);
-            ByID("whois_raw").innerHTML = exception;
-          },
-        });
-      });
-      ByID("button_whois_" + tld).appendChild(Button_Whois);
     }
+
+    const Button_Whois = document.createElement("button");
+    Button_Whois.type = "button";
+    Button_Whois.className = "btn btn-info btn-sm";
+    Button_Whois.setAttribute("data-toggle", "modal");
+    Button_Whois.setAttribute("data-target", "#myModal");
+    Button_Whois.textContent = "Whois";
+    Button_Whois.addEventListener("click", function() {
+      ByID("modal_domain_title").innerHTML = domain;
+      ByID("whois_raw").innerHTML = "...Consultando...";
+      $.ajax({
+        type: "POST",
+        global: false,
+        dataType: "html",
+        url: "/get_whois_raw",
+        data: {domain: domain, csrf_token: odoo.csrf_token},
+        success: function(data) {
+          ByID("whois_raw").innerHTML = data;
+        },
+        error: function(jqxhr, status, exception) {
+          // Alert("Exception:", exception);
+          ByID("whois_raw").innerHTML = exception;
+        },
+      });
+    });
+    ByID("button_whois_" + tld).appendChild(Button_Whois);
+
+
   }
   function whois(domain_name, tld) {
     $.ajax({
@@ -72,7 +73,7 @@ odoo.define("sgw_whois.tools", function(require) {
   function getStatus2(domain_name) {
     ByID("loading_table").style.visibility = "visible";
     function callback(response) {
-      let obj = JSON.parse(response);
+      const obj = JSON.parse(response);
       for (var k in obj) {
         whois(domain_name, obj[k]);
       }
@@ -90,7 +91,7 @@ odoo.define("sgw_whois.tools", function(require) {
   }
 
   if (window.location.pathname === "/whois") {
-    let value_domain = ByID("domain").value;
+    const value_domain = ByID("domain").value;
     if (value_domain !== "") {
       getStatus2(value_domain);
     }
